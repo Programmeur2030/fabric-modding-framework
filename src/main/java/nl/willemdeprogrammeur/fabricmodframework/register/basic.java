ac4c4daf3a53d217c00ecfd610bc03806291b8ff
Programmeur2030 <willem_de_prog@outlook.com>
2025-06-19 14:52:23 +0200

    ik ben weer verder gegaan. ik heb nu heel #2 af

--- src/main/java/nl/willemdeprogrammeur/fabricmodframework/register/basic.java
+++ src/main/java/nl/willemdeprogrammeur/fabricmodframework/register/basic.java
@@ -1,18 +1,50 @@
 package nl.willemdeprogrammeur.fabricmodframework.register;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import nl.willemdeprogrammeur.fabricmodframework.Fmoddingframework;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.RegistryKey;
+
+import java.util.Collections;
+import java.util.List;
 
 public class basic {
-    //items registreren
 
     public static void Register_Basic() {
         Fmoddingframework.LOGGER.info("basis componenten van modding framework aan het laden");
     }
-    public static Item Item(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(Fmoddingframework.MOD_ID, name), item)
+    //items registreren
+    public static Item Item(String name, String MOD_ID, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(MOD_ID, name), item);
+    }
+    // item overload
+    public static Item Item(String name, Item item, RegistryKey<ItemGroup> creativeTab) {
+        Item registered = Registry.register(Registries.ITEM, new Identifier(Fmoddingframework.MOD_ID, name), item);
+
+        // Voeg toe aan creative tab
+        ItemGroupEvents.modifyEntriesEvent(creativeTab).register(entries -> {
+            entries.add(registered);
+        });
+        return registered;
+    }
+
+    // een item/block groep maken
+    public static ItemGroup ItemGroup(String name, String MOD_ID, ItemStack icon, List<ItemStack> items) {
+        Identifier id = new Identifier(MOD_ID, name);
+        return Registry.register(Registries.ITEM_GROUP, id,
+                FabricItemGroup.builder()
+                        .icon(() -> icon)
+                        .displayName(Text.translatable("itemGroup." + MOD_ID + "." + name))
+                        .entries((displayContext, entries) -> {
+                            entries.addAll(Collections.unmodifiableList(items));
+                        })
+                        .build());
     }
 }
